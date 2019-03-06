05273f05f6f5738736d214aaeddea0404f781ce6
Magnus Sundström <magnus.sundstrom@tretton37.com>
2019-03-06 15:12:48 +0100

    Add project files.

---
+++ ProBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
