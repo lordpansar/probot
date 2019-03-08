a38d42d099e70bcb6779d27e788130f7c4505753
Magnus Sundström <magnus.sundstrom@tretton37.com>
2019-03-08 17:24:44 +0100

    Added models

---
+++ ProBot/Direction.cs
@@ -0,0 +1,11 @@
+﻿namespace ProBot
+{
+    public enum Direction
+    {
+        NORTH = 1,
+        EAST = 2,
+        SOUTH = 3,
+        WEST = 4,
+        ILLEGAL = 5
+    }
+}
---
+++ ProBot/Instruction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProBot
+{
+    public class Instruction
+    {
+        public Instruction()
+        {
+            StartPosition = new Position();
+            Direction = new Direction();
+            Moves = new List<string>();
+        }
+
+        public Position StartPosition { get; set; }
+        public Direction Direction { get; set; }
+        public List<string> Moves { get; set; }
+    }
+}
