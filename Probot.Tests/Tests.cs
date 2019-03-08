54af8a5c8fa781d4bc64a1f26d4bf1071d762d3c
Magnus Sundström <magnus.sundstrom@tretton37.com>
2019-03-08 13:55:51 +0100

    Added test for comparing instructions before/after cleaning

--- Probot.Tests/Tests.cs
+++ Probot.Tests/Tests.cs
@@ -1,4 +1,4 @@
-using ProBot;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ProBot.Tests
@@ -19,6 +19,39 @@ namespace ProBot.Tests
             var direction = Program.GetDirection("NORTHWEST");
 
             Assert.Equal(Direction.ILLEGAL, direction);
+        }
+
+        [Fact]
+        public void AssertThatRawInstructionsGetCleaned()
+        {
+            var setup = new List<string> { "PLACE 0,1,EAST", "MOVE", "REPORT" };
+
+            var correctlyCleanedInstruction = new Instruction
+            {
+                Direction = Direction.EAST,
+                Moves = new List<string>(),
+                StartPosition = new Position { Horizontal = 0, Vertical = 1 }
+            };
+
+            var cleanedInstructions = Program.CleanRawInstructions(setup);
+
+            var areEqual = CompareInstructions(cleanedInstructions, correctlyCleanedInstruction);
+
+            Assert.True(areEqual);
+        }
+
+        private bool CompareInstructions(Instruction cleanedInstruction, Instruction correctlyCleanedInstruction)
+        {
+            if(cleanedInstruction.Direction != correctlyCleanedInstruction.Direction)
+                return false;
+            if (cleanedInstruction.Moves.Equals(correctlyCleanedInstruction.Moves))
+                return false;
+            if (cleanedInstruction.StartPosition.Horizontal != correctlyCleanedInstruction.StartPosition.Horizontal)
+                return false;
+            if (cleanedInstruction.StartPosition.Vertical != correctlyCleanedInstruction.StartPosition.Vertical)
+                return false;
+            else
+                return true;
         }
     }
 }
