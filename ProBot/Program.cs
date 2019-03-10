7dd035e5e39e34a57d695056430bf9cf4d7b85f7
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-10 15:15:51 +0100

    Moved movement related logic to separate MovementService class

--- ProBot/Program.cs
+++ ProBot/Program.cs
@@ -6,151 +6,18 @@ namespace ProBot
 {
     public class Program
     {
+
         static void Main(string[] args)
         {
             var table = new int[5,5];
             var instruction = GetInstructions();
 
-            Move(instruction);
+            var movementService = new MovementService();
+
+            movementService.Move(instruction);
         }
 
-        public static void Move(Instruction instructions)
-        {
-            var currentVertical = instructions.StartPosition.Vertical;
-            var currentHorizontal = instructions.StartPosition.Horizontal;
-            var currentDirection = instructions.Direction;
-
-            int nextVertical = 0;
-            int nextHorizontal = 0;
-
-            bool isIllegal;
-
-            //Check if placement is outside the table
-            isIllegal = CheckForIllegalMove(currentVertical, currentHorizontal);
-
-            if(isIllegal)
-            {
-                return;
-            }
-
-            foreach (var instruction in instructions.InstructionsList)
-            {
-                if(instruction == "MOVE")       
-                {       
-                    if(currentDirection == Direction.NORTH)       
-                    {       
-                       nextVertical = currentVertical - 1;       
-                       nextHorizontal = currentHorizontal;       
-
-                       isIllegal = CheckForIllegalMove(nextVertical, nextHorizontal);       
-                    }       
-                    else if (currentDirection == Direction.EAST)       
-                    {       
-                       nextVertical = currentVertical;       
-                       nextHorizontal = currentHorizontal + 1;       
-
-                       isIllegal = CheckForIllegalMove(nextVertical, nextHorizontal);       
-                    }       
-                    else if (currentDirection == Direction.SOUTH)       
-                    {       
-                       nextVertical = currentVertical + 1;       
-                       nextHorizontal = currentHorizontal;       
-
-                       isIllegal = CheckForIllegalMove(nextVertical, nextHorizontal);       
-                    }       
-                    else if (currentDirection == Direction.WEST)       
-                    {       
-                       nextVertical = currentVertical;       
-                       nextHorizontal = currentHorizontal - 1;       
-
-                       isIllegal = CheckForIllegalMove(nextVertical, nextHorizontal);       
-                    }       
-
-                    if(isIllegal)
-                    {
-                       continue;
-                    }
-
-                    if(!isIllegal)       
-                    {       
-                       currentVertical = nextVertical;       
-                       currentHorizontal = nextHorizontal;       
-                       continue;       
-                    }
-                }       
-
-                if(instruction == "REPORT")       
-                {       
-                    Message.PrintReport(currentVertical, currentHorizontal, currentDirection);
-                    continue;
-                }       
-
-                if(instruction == "LEFT" || instruction == "RIGHT")       
-                {       
-                    currentDirection = Turn(instruction, currentDirection);       
-                    continue;       
-                }       
-            }
-        }
-
-        public static bool CheckForIllegalMove(int vertical, int horizontal)
-        {
-            if(horizontal > 5 || horizontal < 0 || vertical > 5 || vertical < 0)
-            {
-                Message.OutOfBounds(vertical, horizontal);
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public static Direction Turn(string turn, Direction currentDirection)
-        {
-            Direction newDirection = new Direction();
-
-            if(turn == "LEFT")
-            {
-                if (currentDirection == Direction.NORTH)
-                {
-                    newDirection = Direction.WEST;
-                }
-                if (currentDirection == Direction.EAST)
-                {
-                    newDirection = Direction.NORTH;
-                }
-                if (currentDirection == Direction.SOUTH)
-                {
-                    newDirection = Direction.EAST;
-                }
-                if (currentDirection == Direction.WEST)
-                {
-                    newDirection = Direction.SOUTH;
-                }
-            }
-            else
-            {
-                if (currentDirection == Direction.NORTH)
-                {
-                    newDirection = Direction.EAST;
-                }
-                if (currentDirection == Direction.EAST)
-                {
-                    newDirection = Direction.SOUTH;
-                }
-                if (currentDirection == Direction.SOUTH)
-                {
-                    newDirection = Direction.WEST;
-                }
-                if (currentDirection == Direction.WEST)
-                {
-                    newDirection = Direction.NORTH;
-                }
-            }
-            
-            return newDirection;
-        }
+        
 
         public static Instruction GetInstructions()
         {
