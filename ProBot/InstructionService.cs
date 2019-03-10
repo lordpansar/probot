b768adf20b6e576fd296da93cf5553c1f3c38509
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-10 15:28:19 +0100

    Moved out instruction logic to separate InstructionService class

---
+++ ProBot/InstructionService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ProBot
+{
+    public class InstructionService
+    {
+        public Instruction GetInstructions()
+        {
+            string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\Instructions.txt"));
+
+            var lineArray = File.ReadAllLines(path);
+            var rawInstructions = new List<string>(lineArray);
+
+            var cleanedInstructions = ParseRawInstructions(rawInstructions);
+
+            return cleanedInstructions;
+        }
+
+        public Instruction ParseRawInstructions(List<string> rawInstructionsList)
+        {
+            //TODO: Skriv klart denna metod så den hanterar alla typer av instruktioner
+            var instruction = new Instruction();
+            var instructionsList = new List<string>();
+
+            foreach (var rawInstruction in rawInstructionsList)
+            {
+                if (rawInstruction.Contains("PLACE"))
+                {
+                    var values = rawInstruction.Split(',');
+
+                    instruction.StartPosition.Vertical = int.Parse(values[0].Substring(values[0].Length - 1));
+                    instruction.StartPosition.Horizontal = int.Parse(values[1]);
+                    instruction.Direction = ParseDirection(values[2]);
+
+                    continue;
+                }
+
+                else
+                {
+                    instructionsList.Add(rawInstruction);
+                    continue;
+                }
+            }
+
+            instruction.InstructionsList = instructionsList;
+
+            return instruction;
+        }
+
+        public Direction ParseDirection(string input)
+        {
+            Direction direction = new Direction();
+
+            switch (input)
+            {
+                case "NORTH":
+                    direction = Direction.NORTH;
+                    break;
+                case "EAST":
+                    direction = Direction.EAST;
+                    break;
+                case "SOUTH":
+                    direction = Direction.SOUTH;
+                    break;
+                case "WEST":
+                    direction = Direction.WEST;
+                    break;
+                default:
+                    direction = Direction.ILLEGAL;
+                    break;
+            }
+
+            return direction;
+        }
+    }
+}
--- ProBot/Program.cs
+++ ProBot/Program.cs
@@ -1,91 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-
 namespace ProBot
 {
     public class Program
     {
-
         static void Main(string[] args)
         {
-            var table = new int[5,5];
-            var instruction = GetInstructions();
-
             var movementService = new MovementService();
+            var instructionService = new InstructionService();
+
+            var instruction = instructionService.GetInstructions();
 
             movementService.Move(instruction);
-        }
-
-        
-
-        public static Instruction GetInstructions()
-        {
-            string path = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\Instructions.txt"));
-
-            var lineArray = File.ReadAllLines(path);
-            var rawInstructions = new List<string>(lineArray);
-
-            var cleanedInstructions = ParseRawInstructions(rawInstructions);
-
-            return cleanedInstructions;
-        }
-
-        public static Instruction ParseRawInstructions(List<string> rawInstructionsList)
-        {
-            //TODO: Skriv klart denna metod så den hanterar alla typer av instruktioner
-            var instruction = new Instruction();
-            var instructionsList = new List<string>();
-
-            foreach (var rawInstruction in rawInstructionsList)
-            {
-                if(rawInstruction.Contains("PLACE"))
-                {
-                    var values = rawInstruction.Split(',');
-                    
-                    instruction.StartPosition.Vertical = int.Parse(values[0].Substring(values[0].Length - 1));
-                    instruction.StartPosition.Horizontal = int.Parse(values[1]);
-                    instruction.Direction = ParseDirection(values[2]);
-                    
-                    continue;
-                }
-
-                else
-                {
-                    instructionsList.Add(rawInstruction);
-                    continue;
-                }
-            }
-
-            instruction.InstructionsList = instructionsList;
-
-            return instruction;
-        }
-
-        public static Direction ParseDirection(string input)
-        {
-            Direction direction = new Direction();
-
-            switch (input)
-            {
-                case "NORTH":
-                    direction = Direction.NORTH;
-                    break;
-                case "EAST":
-                    direction = Direction.EAST;
-                    break;
-                case "SOUTH":
-                    direction = Direction.SOUTH;
-                    break;
-                case "WEST":
-                    direction = Direction.WEST;
-                    break;
-                default:
-                    direction = Direction.ILLEGAL;
-                    break;
-            }
-
-            return direction;
         }
     }
 }
