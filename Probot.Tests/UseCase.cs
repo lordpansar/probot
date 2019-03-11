1b06f587d877f1870c454f57c365f4144eed05f4
Magnus Sundström <kmvsundstrom@gmail.com>
2019-03-11 20:42:57 +0100

    Added more use case tests, renamed class FullTest => UseCase

--- Probot.Tests/UseCase.cs
+++ Probot.Tests/UseCase.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 using Xunit;
 
 namespace ProBot.Tests
@@ -86,6 +84,54 @@ namespace ProBot.Tests
             var report = movementService.Move(parsedInstructions);
 
             Assert.Equal("0,1, EAST", report);
+        }
+
+        [Fact]
+        public void AssertThatSinglePlacementInstructionIsOK()
+        {
+            var rawInstructions = new List<string> { "PLACE 0,0,NORTH" };
+
+            var parsedInstructions = instructionService.ParseRawInstructions(rawInstructions);
+
+            var report = movementService.Move(parsedInstructions);
+
+            Assert.Equal("0,0, NORTH", report);
+        }
+
+        [Fact]
+        public void AssertThatSingleReportInstructionIsOK()
+        {
+            var rawInstructions = new List<string> { "REPORT" };
+
+            var parsedInstructions = instructionService.ParseRawInstructions(rawInstructions);
+
+            var report = movementService.Move(parsedInstructions);
+
+            Assert.Equal("", report);
+        }
+
+        [Fact]
+        public void AssertThatOnlyMultipleReportsAreOK()
+        {
+            var rawInstructions = new List<string> { "REPORT", "REPORT", "REPORT" };
+
+            var parsedInstructions = instructionService.ParseRawInstructions(rawInstructions);
+
+            var report = movementService.Move(parsedInstructions);
+
+            Assert.Equal("", report);
+        }
+
+        [Fact]
+        public void AssertThatMultipleReportsTogetherWithOtherInstructionsAreOK()
+        {
+            var rawInstructions = new List<string> { "PLACE 0,0,NORTH", "MOVE", "REPORT", "PLACE 0,1,EAST", "MOVE", "REPORT" };
+
+            var parsedInstructions = instructionService.ParseRawInstructions(rawInstructions);
+
+            var report = movementService.Move(parsedInstructions);
+
+            Assert.Equal("1,1, EAST", report);
         }
     }
 }
